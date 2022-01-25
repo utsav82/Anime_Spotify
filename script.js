b2c31439424773eaeca8c15e07a53ef215658b65
Malay <malaykumar2003@gmail.com>
2022-01-25 10:41:01 +0530

    saving all changes till now

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
+
+function play_pause_trans(){
+const in_p = document.getElementById("play-pause");
+const p_b = document.getElementById("ch");
+console.log(p_b.d);
+let state = "play";
+in_p.addEventListener('click', ()
+=> {
+  if(state == "play"){
+    p_b.d="M272 63.1l-32 0c-26.51 0-48 21.49-48 47.1v288c0 26.51 21.49 48 48 48L272 448c26.51 0 48-21.49 48-48v-288C320 85.49 298.5 63.1 272 63.1zM80 63.1l-32 0c-26.51 0-48 21.49-48 48v288C0 426.5 21.49 448 48 448l32 0c26.51 0 48-21.49 48-48v-288C128 85.49 106.5 63.1 80 63.1z";
+    state = "pause";
   }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+
+});
+}
+function play() {
+
+}
