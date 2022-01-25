45192dbd9d6e035cd654fa9c05fff368e154eee2
Malay-dev <91375797+Malay-dev@users.noreply.github.com>
2022-01-25 18:09:45 +0530

    latest update after "small clean and player"

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-  let state = "play";
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
+
+
+let state = "play";
 function play_pause_trans(){
-
-
-
 if(state == "play")
 {
   document.getElementById('play_btn_path').setAttribute('d',"M272 63.1l-32 0c-26.51 0-48 21.49-48 47.1v288c0 26.51 21.49 48 48 48L272 448c26.51 0 48-21.49 48-48v-288C320 85.49 298.5 63.1 272 63.1zM80 63.1l-32 0c-26.51 0-48 21.49-48 48v288C0 426.5 21.49 448 48 448l32 0c26.51 0 48-21.49 48-48v-288C128 85.49 106.5 63.1 80 63.1z");
@@ -23,3 +22,4 @@ else{
 }
 console.log(state);
 }
+
